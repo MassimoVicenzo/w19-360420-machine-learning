4d61fa7d22b2477cdcfe728b2fbcd837e19cb46d
Massimo Vicenzo <mastermass8822@gmail.com>
2019-05-11 02:27:37 -0400

    Task 5 and commenting out unnecesary stuff

--- kNNMain.java
+++ kNNMain.java
@@ -14,38 +14,41 @@ public static final double trainfrac = 0.8;
     // TASK 1: Use command line arguments to point DataSet.readDataSet method to
     // the desired file. Choose a given DataPoint, and print its features and label
 
+
     List<DataPoint> List = DataSet.readDataSet(args[0]);
+
+/*
     DataPoint DP = List.get(5);
     double[] x = DP.getX();
     System.out.println(DP.getLabel());
     System.out.println(Arrays.toString(x));
-
+*/
 
     //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
 
     List<DataPoint> test = DataSet.getTestSet(List, testfrac);
     List<DataPoint> train = DataSet.getTrainingSet(List, trainfrac);
 
-    for (int i=0;i<test.size(); i++) {
-      DataPoint DPtest = List.get(i);
-      double[] xtest = DPtest.getX();                 //prints the test set
-      System.out.println(DPtest.getLabel());
-      System.out.println(Arrays.toString(xtest));
-
-    }
 
     // TASK 4: write a new method in DataSet.java which takes as arguments to DataPoint objects,
     // and returns the Euclidean distance between those two points (as a double)
 
-  System.out.print(DataSet.distanceEuclid(test.get(1), test.get(0)));  // TEST: prints distance between two values
+//  System.out.print(DataSet.distanceEuclid(test.get(1), test.get(0)));  // TEST: prints distance between two values
 
     // TASK 5: Use the KNNClassifier class to determine the k nearest neighbors to a given DataPoint,
     // and make a print a predicted target label
 
+    KNNClassifier k = new KNNClassifier(3);
 
+/*
+    String prediction = k.predict(List, DP);
+    System.out.println(prediction);
+*/
 
     // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
     // point based on nearest neighbors in training set. Calculate accuracy of model.
+
+
 
 
   }
